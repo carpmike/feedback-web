423fb5a8c1e3dfb907d134caed0da4f984e5e725
Mike Carpenter <mcarpenter@summa-tech.com>
2014-01-27 08:03:55 -0500

    got put working for feedback update

--- grails-app/domain/feedback/web/FeedbackType.groovy
+++ grails-app/domain/feedback/web/FeedbackType.groovy
@@ -1,10 +1,13 @@
 package feedback.web;
 
+import grails.rest.Resource;
+
+@Resource(uri='/feedbackTypes', formats=['json', 'xml'])
 public class FeedbackType {
 	
 	String name
 	
 	static constraints = {
-		name inList: ["positive", "negative"]
+		name inList: ["positive", "constructive"]
 	}
 }
